cb648cd9c117ec9b8bfbedd7eb661ad0fef4c77e
vamartid <vamartid@yandex.com>
2016-07-02 12:59:36 +0300

    added License

--- JmailProject_CLIENT/src/vamartid/test1/_email/MY_socket_CLIENT.java
+++ JmailProject_CLIENT/src/vamartid/test1/_email/MY_socket_CLIENT.java
@@ -1,6 +1,12 @@
 package vamartid.test1._email;
 //——————————————————————————————————————————————————————————————————————————————————————
-
+/*
+ * License Copyright © 2000 Vasileios Martidis vamartid@yandex.com 
+ * This work is free. You can redistribute it and/or modify it under
+ *  the terms of the Do What The Fuck You Want To Public License,
+ *  Version 2, as published by Sam Hocevar.
+ *  See http://www.wtfpl.net/ for more details.
+ */
 import java.io.*;
 import java.net.*;
 import java.io.ObjectInputStream;
